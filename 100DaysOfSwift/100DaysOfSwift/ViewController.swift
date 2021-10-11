c9ddf78a472e4821593be2bf97fe94a49a7822a9
Kirill <Kirill07478@gmail.com>
2021-10-11 15:50:20 +0300

    Third Day

--- 100DaysOfSwift/100DaysOfSwift/ViewController.swift
+++ 100DaysOfSwift/100DaysOfSwift/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        SecondDay.secondDayCheck()
+        //FirstDay.firstDayCheck()
+        //SecondDay.secondDayCheck()
+        ThirdDay.thirdDayCheck()
     }
 
 
